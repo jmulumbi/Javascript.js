144f72f2d7948fc4179d4d24822bfbe9f0359359
Jeremiah Mulumbi <124074964+jmulumbi@users.noreply.github.com>
2024-01-15 20:07:32 +0300

    Create Template Literals and Strings.js

---
+++ Template Literals and Strings.js
@@ -0,0 +1,16 @@
+console.log("I'm happy")
+/**Line Breaking*/
+console.log("Dont ask if im done yet \n ill grind till sunset to \n so momma knows her sons set");
+/**Tab*/
+console.log("\t hey \t there")
+/** Template Literals- Using backticks*/
+console.log(`Hello how are you
+are you okay `)
+let name = "Josh"
+let text = `Welcome, ${name}`
+console.log(text);
+/**Concatenation*/
+let x = "Java" 
+let y = "Script";
+let text = x + y;
+console.log(text);
