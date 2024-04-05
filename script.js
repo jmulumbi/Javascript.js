edb8d7d6ada070c0468007c1d45032f58137e1a1
Jeremiah Mulumbi <124074964+jmulumbi@users.noreply.github.com>
2024-04-05 16:10:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+//Lesson5:Operators 
+//Addtion 
+let a = 5 + 10;
+console.log(a);
+//Subtraction
+let b = 10 - 5;
+console.log(b);
+//Multiplication
+let c = 5*5;
+console.log(c);
+//Division
+let d = 5/2;
+console.log(d);
+//Modulus
+let e = 6 % 4;
+console.log(e);
+//Exponent
+let f = 5**2;
+console.log(f);
+
+//Assignment Operators
+let g = 4;
+g+=4; //adds four
+g*=4; //multiplies by 4
+g/=4; //division by 4
+g**=5; //4^5
+console.log(g);
+
+//Increment Operator 
+let h = 5;
+h++;//adds 1 
+h--;//subtracts 1
+console.log(h)
+
+//Post Fix Operators
+
+//Pre Fix Operators
+let i = 10;
+console.log(++i)//Incremented by 1 first then put on the console
+let j = 5;
+console.log(j++);
+console.log();
+
+//Comparison Operators
+//<
+//>
+// <=
+// >=
+// == Looks at value
+// !=
+//Strict Equality Check ==== Checks data types
+//Strict Inequality Check !===
+
+//Logical Operataors 
+//AND (&&)
+//OR(||)
+//NOt(!)
+
+//String Operators
+console.log("They said time was supposed to heal ya " + "but I aint done much healing");
+let k = "Javascript";
+k+= ' Tut'
+
+//Operator Precedence 
+//Brackets
+//Exponents
+//Division
+//Multiplication
+//Addition
+//Substraction
+
+//Operator Associoativity 
