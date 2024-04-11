b363a2bb773488db0be781df63dc13002c43c633
Jeremiah Mulumbi <124074964+jmulumbi@users.noreply.github.com>
2024-04-11 16:35:47 +0300

    Update RPG game.js
    This is a role playing game that I am creating using the freeCodeCamp free to learn JavaScript Tutorial.

--- RPG game.js
+++ RPG game.js
@@ -20,7 +20,14 @@ const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon]
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
   }
 ];
 
@@ -30,10 +37,6 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-
-}
-
-function goTown() {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
@@ -43,14 +46,12 @@ function goTown() {
   text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
+function goTown() {
+  update(locations);
+}
+
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+
 }
 
 function goCave() {
@@ -67,4 +68,4 @@ function buyHealth() {
 
 function buyWeapon() {
 
-}RPG
+}
